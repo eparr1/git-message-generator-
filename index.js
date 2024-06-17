8d99eaf90534dd013907296eca320551962d16a6
Emma Code's <emmaparr12345@gmail.com>
2024-06-17 19:11:27 +0100

    Fix bug in "feature_x" and improve error handling

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const careerarr = ['Doctor', 'Actor', 'Unemployed', 'Bin man/woman', 'Bricklayer', 'Scientist', 'Office Worker']
-const kidsarr = ['Slug', 'Noah', 'Emily', 'Hugp', 'Charlotte', 'Dontae', 'Margaret'];
-const emotionarr = ['Sad', 'Lonely', 'Happy', 'Smug', 'Jealous', 'Overjoyed', 'Content']
-
 
 const randomMessage = arr => {
     const randomGenerator = Math.floor(Math.random()* arr.length);
@@ -9,4 +5,30 @@ const randomMessage = arr => {
     return randomElement 
 }
 
-console.log(randomMessage(careerarr))
+const messageGenerator = {
+    careerarr: ['Doctor', 'Actor', 'Unemployed', 'Bin man/woman', 'Bricklayer', 'Scientist', 'Office Worker', 'Full stack Developer'],
+    kidsarr: ['Slug', 'Noah', 'Emily', 'Hugo', 'Charlotte', 'Dontae', 'Margaret'],
+    emotionarr: ['Sad', 'Lonely', 'Happy', 'Smug', 'Jealous', 'Overjoyed', 'Content'],
+}
+let futureAnswers = []
+for (let prop in messageGenerator){
+    let generated = randomMessage(messageGenerator[prop])
+    switch (prop){
+        case 'careerarr':
+            futureAnswers.push(`You're future career will be ${generated}`)
+        break
+        case 'kidsarr':
+            futureAnswers.push(`You're future kid's name will be ${generated}`)
+        break 
+        case 'emotionarr':
+            futureAnswers.push(`You're emotions will mainly be ${generated}`)
+        break
+        default:
+            futureAnswers.push('Do you really not want to know?')
+        break
+    }
+}
+
+console.log(futureAnswers)
+
+
